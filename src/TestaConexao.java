44c387a78883f33c80b97cb53daf86cb7871424f
marcb <marcbitenl@gmail.com>
2022-06-02 20:00:25 -0300

    A primeira conexao

---
+++ src/TestaConexao.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC","root","Mark8129");
+
+		
+		System.out.println("Fechando conexão");
+		
+		connection.close();
+		
+		
+	}
+
+}
