0d9bd6884c573a2aa74413db274989e230f2d16e
marcb <marcbitenl@gmail.com>
2022-06-03 19:50:36 -0300

    Assumindo o controle

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -8,17 +8,24 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 
-		String nome = "Mouse'";
-		String descricao = "mouse sem fio";
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
+		connection.setAutoCommit(false);
 
-		;
 		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
 				Statement.RETURN_GENERATED_KEYS);
 
+		adcionarVariavel("SmartTV", "45 polegadas", stm);
+		adcionarVariavel("Radio", "Radio de bateria", stm);
+	}
+
+	private static void adcionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
+
+//		if (nome.equals("Radio")) {
+//			throw new RuntimeException("Não foi possivel adcionar o produto");
+//		}
 
 		stm.execute();
 
@@ -27,6 +34,7 @@ public class TestaInsercaoComParametro {
 			Integer id = rst.getInt(1);
 			System.out.println("O id criado foi: " + id);
 		}
+		rst.close();
 	}
 
 }
--- src/TestaRemocao.java
+++ src/TestaRemocao.java
@@ -9,7 +9,7 @@ public class TestaRemocao {
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
 
-		PreparedStatement stm = connection.prepareStatement("DELETE FROM PRODUTO WHERE ID > 2");
+		PreparedStatement stm = connection.prepareStatement("DELETE FROM PRODUTO WHERE ID > ?");
 		stm.setInt(1, 2);
 		stm.execute();
 
