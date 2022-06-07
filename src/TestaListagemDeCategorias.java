e3515ba6ba7609095c90576ba20361bd5f28ff26
marcb <marcbitenl@gmail.com>
2022-06-07 20:58:03 -0300

    Usando inner Join

--- src/TestaListagemDeCategorias.java
+++ src/TestaListagemDeCategorias.java
@@ -2,11 +2,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
-import com.mchange.v2.sql.filter.SynchronizedFilterDataSource;
-
 import br.com.alura.jdbc.ConnectionFactory;
 import br.com.alura.jdbc.dao.CategoriaDAO;
-import br.com.alura.jdbc.dao.ProdutoDAO;
 import br.com.alura.jdbc.modelo.Categoria;
 import br.com.alura.jdbc.modelo.Produto;
 
@@ -16,17 +13,12 @@ public class TestaListagemDeCategorias {
 
 		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
 			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
-			List<Categoria> listaDeCategorias = categoriaDAO.listar();
+			List<Categoria> listaDeCategorias = categoriaDAO.listarComProdutos();
 			listaDeCategorias.stream().forEach(ct -> {
 				System.out.println(ct.getNome());
-				try {
-					for(Produto produto : new ProdutoDAO(connection).buscar(ct)) {
-						System.out.println(ct.getNome() + " - " + produto.getNome());
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
+				for (Produto produto : ct.getProdutos(ct)) {
+					System.out.println(ct.getNome() + " - " + produto.getNome());
 				}
-				
 			});
 
 		}
--- src/br/com/alura/jdbc/modelo/Categoria.java
+++ src/br/com/alura/jdbc/modelo/Categoria.java
@@ -1,9 +1,13 @@
 package br.com.alura.jdbc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
 
 	private Integer id;
 	private String nome;
+	private List<Produto> produtos = new ArrayList<Produto>();
 
 	public Categoria(Integer id, String nome) {
 		this.id = id;
@@ -14,15 +18,16 @@ public class Categoria {
 		return nome;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 	public int getId() {
 		return id;
 	}
-	
-	
-	
 
+	public void adicionar(Produto produto) {
+		produtos.add(produto);
+	}
+
+	public List<Produto> getProdutos(Categoria ct) {
+		return produtos;
+	}
 }
+
